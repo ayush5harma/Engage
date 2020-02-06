4e2c614fb186b7d5a4d17c0ac9c11133e2ff2398
ayuu5h <47772616+ayuu5h@users.noreply.github.com>
2020-02-07 00:28:44 +0530

    Add files via upload

---
+++ config/secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  //database: 'mongodb://root:abc123@ds037095.mongolab.com:37095/ecommerce',
+  database: 'mongodb://ayush:sjOU7xRWIfnehvWX@cluster0-shard-00-00-k1dtf.azure.mongodb.net:27017,cluster0-shard-00-01-k1dtf.azure.mongodb.net:27017,cluster0-shard-00-02-k1dtf.azure.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  port: 3000,
+  secretKey: "Ayush@$@!#@"
+}
