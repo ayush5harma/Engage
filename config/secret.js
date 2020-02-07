6b663eb7bf83808e738f594c852408841abfd91f
Ayush Kumar Sharma <47772616+ayuu5h@users.noreply.github.com>
2020-02-07 14:04:18 +0530

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,7 +1,6 @@
 module.exports = {
 
-  //database: 'mongodb://root:abc123@ds037095.mongolab.com:37095/ecommerce',
-  database: 'mongodb://ayush:sjOU7xRWIfnehvWX@cluster0-shard-00-00-k1dtf.azure.mongodb.net:27017,cluster0-shard-00-01-k1dtf.azure.mongodb.net:27017,cluster0-shard-00-02-k1dtf.azure.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  //database: 'mongodb azure api key here',
   port: 3000,
-  secretKey: "Ayush@$@!#@"
+  secretKey: ""
 }
